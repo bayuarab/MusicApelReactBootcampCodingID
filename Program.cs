598bf93eb792e885c027a9954380dd250243c646
Bayu Aji Dwisanjaya <arabdoi88@gmail.com>
2022-08-04 08:25:44 +0700

    Cleaning up, and add corspolicy localhost:3000

--- Program.cs
+++ Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<DataContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+   build.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+}));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("corspolicy");
 
 app.UseHttpsRedirection();
 
