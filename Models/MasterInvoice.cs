1d66b24f8c91bffcf3c2602287cc620dcb4311fa
Bayu Aji Dwisanjaya <arabdoi88@gmail.com>
2022-08-21 08:43:20 +0700

    Delete commented line on Invoices model amd user model

--- Models/MasterInvoice.cs
+++ Models/MasterInvoice.cs
@@ -5,7 +5,6 @@ namespace SecondV.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Not valid data")]
         [StringLength(50, MinimumLength = 8, ErrorMessage = "Not valid data")]
-        // [RegularExpression(".*[a-zA-Z]+.*", ErrorMessage = "Not valid data")]
         public string? NoInvoice { get; set; }
         [Required]
         public string? PurchaseDate { get; set; }
--- Models/User.cs
+++ Models/User.cs
@@ -5,10 +5,7 @@ namespace SecondV.Models
         public int Id { get; set; }
         public string? nama { get; set; }
         public string? email { get; set; }
-        // public string? password { get; set; }
-        // [Required(ErrorMessage = "Not valid data")]
         public byte[]? passwordHash { get; set; }
-        // [Required(ErrorMessage = "Not valid data")]
         public byte[]? passwordSalt { get; set; }
         public string roles { get; set; } = "student";
     }
